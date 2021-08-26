93a986b95a02e727f0842a28a5d4d11bd0f97271
Aixcc <2285726233@qq.com>
2021-08-26 15:43:07 +0800

    项目初始化-调整初始化目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
   <div id="app">
     <h1>黑马头条</h1>
+
     <!-- 路由出口 -->
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <script>
-
 </script>
 
 <style lang="less">
-
 </style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,13 @@ import store from './store'
 // 引入全局样式
 import './styles/index.less'
 
+// 配制 vant组件库
+import Vant from 'vant'
+// 引入组件库样式
+import 'vant/lib/index.css'
+//  使用组件库
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
